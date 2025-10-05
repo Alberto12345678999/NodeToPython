b2d4c652324d34a992070668f90e2aeeaf064952
BrendanParmer <51296046+BrendanParmer@users.noreply.github.com>
2025-10-04 19:44:14 -0500

    refactor: main menu naming consistency

--- NodeToPython/ntp_menu.py
+++ NodeToPython/ntp_menu.py
@@ -1,7 +1,7 @@
 import bpy
 
-class NodeToPythonMenu(bpy.types.Menu):
-    bl_idname = "NODE_MT_node_to_python"
+class NTP_MT_NodeToPython(bpy.types.Menu):
+    bl_idname = "NTP_MT_node_to_python"
     bl_label = "Node To Python"
 
     @classmethod
@@ -11,3 +11,7 @@ class NodeToPythonMenu(bpy.types.Menu):
     def draw(self, context):
         layout = self.layout.column_flow(columns=1)
         layout.operator_context = 'INVOKE_DEFAULT'
+
+classes = [
+    NTP_MT_NodeToPython
+]
